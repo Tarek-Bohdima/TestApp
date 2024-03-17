f0ed904aa751c70e1c4c4d3e479a1393ccc6bd4e
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2024-03-17 17:26:28 +0200

    apply new activity for result

--- app/src/main/java/com/tarek/testapp/MainActivity.kt
+++ app/src/main/java/com/tarek/testapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.tarek.testapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,30 +11,39 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.tarek.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
 private lateinit var binding: ActivityMainBinding
+private val startActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {acitivtyResult ->
+    if (acitivtyResult != null && acitivtyResult.resultCode == RESULT_OK) {
+        if ((acitivtyResult.data != null) && (acitivtyResult.data!!.getStringExtra(MainActivity2.KEY_NAME) != null)) {
+            binding.receivedText.text = acitivtyResult.data!!.getStringExtra(
+                MainActivity2.KEY_NAME
+            )
+        }
+    }
+}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle("First Activity")
 
      binding = ActivityMainBinding.inflate(layoutInflater)
      setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
+        binding.button.setOnClickListener {view ->
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivityForResult.launch(intent)
         }
+
     }
 override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,9 +61,4 @@ override fun onCreateOptionsMenu(menu: Menu): Boolean {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
-    }
 }
